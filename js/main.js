2bda3a6bd254845a28af8d7ac167349bae20d7b1
prokutin <q152827060@yandex.ru>
2022-10-01 21:13:40 +0300

    Добавлены функции getRandomNumber и checkLengthString в main.js

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+
+function getRandomNumber(min, max) {
+
+  if (Math.sign(min) === -1 || Math.sign(max) === -1 || min === max) {
+    return NaN;
+  }
+
+  if (max < min) {
+    const changeNumber = max;
+    max = min;
+    min = changeNumber;
+  }
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+  // использован материал https://habr.com/ru/post/312880/
+}
+
+function checkLengthString (checkString, maxLength) {
+  if (checkString.length > maxLength) {
+    return true;
+  }
+  return false;
+}
+
+getRandomNumber(1, 10);
+checkLengthString ('какой то текст', 50);
+
