4cac6ccdfc9d0335e87f9be0b901f3a3181ee120
Keks <keks@htmlacademy.ru>
2022-10-02 21:13:48 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/main.js
+++ 3/js/main.js
@@ -1,27 +1,14 @@
+const getRandomNumber = (min, max) => {
 
-
-function getRandomNumber(min, max) {
-
-  if (Math.sign(min) === -1 || Math.sign(max) === -1 || min === max) {
+  if (min < 0 || min > max) {
     return NaN;
-  }
-
-  if (max < min) {
-    const changeNumber = max;
-    max = min;
-    min = changeNumber;
   }
 
   return Math.floor(Math.random() * (max - min + 1)) + min;
   // использован материал https://habr.com/ru/post/312880/
-}
+};
 
-function checkLengthString (checkString, maxLength) {
-  if (checkString.length < maxLength) {
-    return true;
-  }
-  return false;
-}
+const checkLengthString = (checkString, maxLength) => checkString.length < maxLength;
 
 getRandomNumber(1, 10);
 checkLengthString ('какой то текст ', 50);
