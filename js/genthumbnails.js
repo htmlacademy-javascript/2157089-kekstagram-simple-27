1d366ece4ab46d3a4e611bee1b6ba7f0aea8ccc2
prokutin <q152827060@yandex.ru>
2022-11-16 11:16:56 +0300

    final

--- js/genthumbnails.js
+++ js/genthumbnails.js
@@ -1,4 +1,4 @@
-import { getData } from './api.js';
+import {getData} from './api.js';
 
 const picturesUsersBlock = document.querySelector('.pictures__title');
 picturesUsersBlock.classList.remove('visually-hidden');
